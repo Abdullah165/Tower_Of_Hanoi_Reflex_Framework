21b7b9a76a57395ca37c55ee8af1f0a43ca8d82c
Abdullah Abdelaty <63372032+Abdullah165@users.noreply.github.com>
2025-12-31 19:39:47 +0200

    Draggable Controller

---
+++ Assets/Scripts/Managers/DraggableController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private float zCoord;
+
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        rigidbody.isKinematic = true;
+
+        zCoord = Camera.main.WorldToScreenPoint(transform.position).z;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 currentPos = GetMouseWorldPos();
+
+        transform.position = currentPos;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        rigidbody.isKinematic = false;
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = zCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+}
